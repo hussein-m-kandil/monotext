97bbf235b8752c9893a93af89157661eb8ec00f6
Hussein Kandil <hussein.mahmoud.s7s@gmail.com>
2023-08-20 01:00:51 +0300

    Logic for adding a new comment on a post 🚀🚀🚀

--- posts/static/js/main.js
+++ posts/static/js/main.js
@@ -1,7 +1,7 @@
 const serverErrorHTMLMessage = () => {
   return `
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>Sorry</strong>, <em>we cannot publish your post right now!</em><br>
+            <strong>Sorry</strong>, <em>we cannot proceed your request right now!</em><br>
             <strong>Please try again later.</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
             </button>
@@ -9,9 +9,55 @@ const serverErrorHTMLMessage = () => {
     `;
 };
 
+const getPostCommentsChunk = async (
+  url,
+  postID,
+  counterHTMLElement,
+  errorsDiv,
+  newCommentAdded = false
+) => {
+  try {
+    const response = await fetch(url, { method: "GET" });
+    if (response.ok) {
+      const data = await response.json();
+      if (data.commentsChunk) {
+        const postCommentsDiv = document.getElementById(
+          "post-comments-" + postID
+        );
+        if (newCommentAdded) {
+          postCommentsDiv.innerHTML += `
+            <div class="h6">${data.commentsChunk[0].text}</div>
+          `;
+        } else {
+          for (let i = 0; i < data.commentsChunk.length; i++) {
+            postCommentsDiv.innerHTML += `
+              <div class="h6">${data.commentsChunk[i].text}</div>
+            `;
+          }
+        }
+        if (data.hasNext) {
+          counterHTMLElement.innerHTML =
+            Number(counterHTMLElement.innerHTML) + 1;
+        } else {
+          counterHTMLElement.innerHTML = 1;
+        }
+        return true;
+      }
+    } else {
+      console.log("Response =>\n", response);
+      errorsDiv.innerHTML += serverErrorHTMLMessage();
+      return false;
+    }
+  } catch (error) {
+    console.log("Error =>\n", error);
+    return false;
+  }
+};
+
 const formHandler = async (
   url,
   form,
+  postID,
   errorsDiv,
   textField,
   titleField = false
@@ -23,23 +69,42 @@ const formHandler = async (
       body: formData,
     });
     if (response.redirected) {
-      window.location.href = response.url;
+      if (titleField) {
+        // It was a new post submission
+        window.location.href = response.url;
+      } else {
+        // It was a new comment submission
+        try {
+          const commentsPaginationCounter = document.getElementById(
+            "comments-pagination-counter-" + postID
+          );
+          const postCommentsURL = document.getElementById(
+            "post-comments-url-" + postID
+          ).innerText;
+          getPostCommentsChunk(
+            postCommentsURL,
+            postID,
+            commentsPaginationCounter,
+            errorsDiv,
+            true
+          );
+        } catch (e) {
+          errorsDiv.innerHTML += serverErrorHTMLMessage();
+          console.log("Error =>\n", e);
+          setTimeout(() => {}, 5000);
+          window.location.href = response.url;
+          return false;
+        }
+      }
+      form.elements.text.value = "";
     } else if (response.ok) {
       const data = await response.json();
       if (data.text && data.text.length > 0) {
-        textField.classList.add("is-invalid");
-        for (let i = 0; i < data.text.length; i++) {
-          errorsDiv.innerHTML += `
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            ${data.text[i]}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                            </button>
-                        </div>
-                    `;
+        if (titleField) {
+          titleField.classList.add("is-invalid");
+        } else {
+          textField.classList.add("is-invalid");
         }
-      }
-      if (titleField && data.title && data.title.length > 0) {
-        titleField.classList.add("is-invalid");
         for (let i = 0; i < data.text.length; i++) {
           errorsDiv.innerHTML += `
                         <div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -66,22 +131,28 @@ const formHandler = async (
 const postForm = document.getElementById("post-form");
 postForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const errorsDiv = document.getElementById("post-form-errors");
-  const textField = document.getElementById("post-text");
-  const titleField = document.getElementById("post-title");
-  formHandler(
-    event.target.action,
-    event.target,
-    errorsDiv,
-    textField,
-    titleField
-  );
+  const form = event.target;
+  const url = form.action;
+  const postID = form.elements.postID.value;
+  const errorsDiv = form.elements.errorsDiv;
+  const textField = form.elements.text;
+  const titleField = form.elements.title;
+  formHandler(url, form, postID, errorsDiv, textField, titleField);
 });
 
-const commentForm = document.getElementById("comment-form");
-commentForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const errorsDiv = document.getElementById("comment-form-errors");
-  const textField = document.getElementById("comment-text");
-  formHandler(event.target.action, event.target, errorsDiv, textField);
-});
+try {
+  const commentFormsList = document.getElementsByClassName("comment-form");
+  for (let i = 0; i < commentFormsList.length; i++) {
+    commentFormsList[i].addEventListener("submit", (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const url = form.action;
+      const postID = form.elements.postID.value;
+      const errorsDiv = form.elements.errorsDiv;
+      const textField = form.elements.text;
+      formHandler(url, form, postID, errorsDiv, textField);
+    });
+  }
+} catch (e) {
+  console.log("Error =>\n", e);
+}
